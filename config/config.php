8ca37c6d283ca4af50bf8e317c242497a7dde0a4
ais <man.human9090@gmail.com>
2022-11-11 09:39:38 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'occ1ebe66d71',
+  'passwordsalt' => '9043848e82c238df0c37c6ba580b78',
+  'secret' => '6890f62afb66545883c9c90123ff487e7a707bec53c5930a74a666796493a9639a9f5fb7537db743aadd5b99510a6cad',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => 'C:\\xamppp\\htdocs\\owncloud\\data',
+  'overwrite.cli.url' => 'http://localhost/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '7.0.15.2',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => 'e7d4d322e9e68beefed8bb11f5e3fd',
+  'installed' => true,
+);
---
+++ version.php
@@ -0,0 +1,6 @@
+<?php 
+$OC_Version = array(7,0,15,2);
+$OC_VersionString = '7.0.15';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_Build = '2016-05-12T19:31:22+00:00 5b012fdebf39ef00eca4b9156a9aa685083aa5de';
